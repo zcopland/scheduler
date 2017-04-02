0eba4bed1f4b78ab592345be2b738e9f21e3a43c
Zach <zachcopland@tarzan-korleh.ad.stetson.edu>
2017-04-02 15:56:31 -0400

    upload
    had to take out sensitive info

---
+++ sendnotifications.php
@@ -0,0 +1,396 @@
+<?php
+    //Twilio requirements
+    require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
+    use Twilio\Rest\Client;
+    
+    //PHPMailer requirements
+    date_default_timezone_set('America/New_York');
+    require '../PHPMailer/PHPMailerAutoload.php';
+    
+    include 'db/dbh.php';
+    
+    /* PHPMailer Code */
+    
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer();
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 0;
+    //Ask for HTML-friendly debug output
+    $mail->Debugoutput = 'html';
+    //Set the hostname of the mail server
+    $mail->Host = "smtp.gmail.com";
+    //enable this if you are using gmail smtp, for mandrill app it is not required
+    $mail->SMTPSecure = 'tls';
+    //Set the SMTP port number - likely to be 25, 465 or 587
+    $mail->Port = 587;
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication
+    $mail->Username = "zcopland16@gmail.com";
+    //Password to use for SMTP authentication
+    $mail->Password = "zksfrfdddeczaxzy";
+    //Blank to email
+    $mail->addAddress('');
+    
+    $method = $_POST['how-update'];
+    
+    /* Twilio Code */
+    
+    //get the body set by the admin
+    $body = $_POST['message'];
+    //get the employees who were check off
+    $em = $_POST['check_employees'];
+    $count_em = count($em); //length of array
+    $people = array("key1" => "value1"); //set dummy value
+    unset($people['key1']); //remove dummy value
+    //loop through array and set it to people array
+    for ($i=0; $i < $count_em; $i++) { 
+        //get the info for the i-th employee
+        $sql = "SELECT * FROM employees WHERE firstName='{$em[$i]}'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        //get full name
+        $fullName = $em[$i];
+        $fullName .= ' ';
+        $fullName .= $row['lastName'];
+        //get email
+        $email = $row['email'];
+        //set beginning of phone number
+        $phone = '+1';
+        //get phone number
+        $phone .= $row['phone'];
+        //set people variable for Twilio
+        $people[$phone] = $em[$i];
+        //Set who the message is to be sent to
+        //but first, check to make sure they actually have an email
+        if (!empty($email)) {
+            $mail->addBCC($email, $fullName);
+        }
+        
+    }
+    
+    // Step 2: set our AccountSid and AuthToken from https://twilio.com/console
+    $AccountSid = "AC3838f81fcd3dbc7f24fb25695b4137b9";
+    $AuthToken = "98a69e68f2c351d86fd0a3e7c260abbc";
+
+    // Step 3: instantiate a new Twilio Rest Client
+    $client = new Client($AccountSid, $AuthToken);
+
+    // Step 5: Loop over all our friends. $number is a phone number above, and 
+    // $name is the name next to it
+    if ($method == 'both' || $method == 'text') {
+            foreach ($people as $number => $name) {
+            $sms = $client->account->messages->create(
+    
+                // the number we are sending to - Any phone number
+                $number,
+    
+                array(
+                    // Step 6: Change the 'From' number below to be a valid Twilio number 
+                    // that you've purchased
+                    'from' => "+12076185206", 
+                    
+                    // the sms body
+                    'body' => "Hello $name,\n" . $body . "\n--Sent from Windham Mall"
+                )
+            );
+    
+            // Display a confirmation message on the screen
+            echo "<h3>Sent message to $name</h3>";
+        }
+    }
+
+
+/* PHPMailer Code */
+$body = nl2br($body);
+$message = <<<HTML
+<head>
+    <meta charset="utf-8"> <!-- utf-8 works for most cases -->
+    <meta name="viewport" content="width=device-width"> <!-- Forcing initial-scale shouldn't be necessary -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- Use the latest (edge) version of IE rendering engine -->
+    <meta name="x-apple-disable-message-reformatting">  <!-- Disable auto-scale in iOS 10 Mail entirely -->
+    <title>Windham Mall Update</title> <!-- The title tag shows in email notifications, like Android 4.4. -->
+
+    <!-- Web Font / @font-face : BEGIN -->
+	<!-- NOTE: If web fonts are not required, lines 10 - 27 can be safely removed. -->
+
+    <!-- Desktop Outlook chokes on web font references and defaults to Times New Roman, so we force a safe fallback font. -->
+    <!--[if mso]>
+        <style>
+            * {
+                font-family: sans-serif !important;
+            }
+        </style>
+    <![endif]-->
+
+    <!-- All other clients get the webfont reference; some will render the font and others will silently fail to the fallbacks. More on that here: http://stylecampaign.com/blog/2015/02/webfont-support-in-email/ -->
+    <!--[if !mso]><!-->
+        <!-- insert web font reference, eg: <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'> -->
+    <!--<![endif]-->
+
+    <!-- Web Font / @font-face : END -->
+
+	<!-- CSS Reset -->
+    <style>
+
+        /* What it does: Remove spaces around the email design added by some email clients. */
+        /* Beware: It can remove the padding / margin and add a background color to the compose a reply window. */
+        html,
+        body {
+            margin: 0 auto !important;
+            padding: 0 !important;
+            height: 100% !important;
+            width: 100% !important;
+        }
+
+        /* What it does: Stops email clients resizing small text. */
+        * {
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%;
+        }
+
+        /* What it does: Centers email on Android 4.4 */
+        div[style*="margin: 16px 0"] {
+            margin:0 !important;
+        }
+
+        /* What it does: Stops Outlook from adding extra spacing to tables. */
+        table,
+        td {
+            mso-table-lspace: 0pt !important;
+            mso-table-rspace: 0pt !important;
+        }
+
+        /* What it does: Fixes webkit padding issue. Fix for Yahoo mail table alignment bug. Applies table-layout to the first 2 tables then removes for anything nested deeper. */
+        table {
+            border-spacing: 0 !important;
+            border-collapse: collapse !important;
+            table-layout: fixed !important;
+            margin: 0 auto !important;
+        }
+        table table table {
+            table-layout: auto;
+        }
+
+        /* What it does: Uses a better rendering method when resizing images in IE. */
+        img {
+            -ms-interpolation-mode:bicubic;
+        }
+
+        /* What it does: A work-around for iOS meddling in triggered links. */
+        *[x-apple-data-detectors] {
+            color: inherit !important;
+            text-decoration: none !important;
+        }
+
+        /* What it does: A work-around for Gmail meddling in triggered links. */
+        .x-gmail-data-detectors,
+        .x-gmail-data-detectors *,
+        .aBn {
+            border-bottom: 0 !important;
+            cursor: default !important;
+        }
+
+        /* What it does: Prevents Gmail from displaying an download button on large, non-linked images. */
+        .a6S {
+	        display: none !important;
+	        opacity: 0.01 !important;
+        }
+        /* If the above doesn't work, add a .g-img class to any image in question. */
+        img.g-img + div {
+	        display:none !important;
+	   	}
+
+        /* What it does: Prevents underlining the button text in Windows 10 */
+        .button-link {
+            text-decoration: none !important;
+        }
+
+        /* What it does: Removes right gutter in Gmail iOS app: https://github.com/TedGoas/Cerberus/issues/89  */
+        /* Create one of these media queries for each additional viewport size you'd like to fix */
+        /* Thanks to Eric Lepetit @ericlepetitsf) for help troubleshooting */
+        @media only screen and (min-device-width: 375px) and (max-device-width: 413px) { /* iPhone 6 and 6+ */
+            .email-container {
+                min-width: 375px !important;
+            }
+        }
+
+    </style>
+
+    <!-- What it does: Makes background images in 72ppi Outlook render at correct size. -->
+	<!--[if gte mso 9]>
+	<xml>
+		<o:OfficeDocumentSettings>
+			<o:AllowPNG/>
+			<o:PixelsPerInch>96</o:PixelsPerInch>
+		</o:OfficeDocumentSettings>
+	</xml>
+	<![endif]-->
+
+    <!-- Progressive Enhancements -->
+    <style>
+
+        /* What it does: Hover styles for buttons */
+        .button-td,
+        .button-a {
+            transition: all 100ms ease-in;
+        }
+        .button-td:hover,
+        .button-a:hover {
+            background: #555555 !important;
+            border-color: #555555 !important;
+        }
+
+    </style>
+
+</head>
+<body width="100%" bgcolor="#222222" style="margin: 0; mso-line-height-rule: exactly;">
+    <center style="width: 100%; background: #222222; text-align: left;">
+
+        <!-- Visually Hidden Preheader Text : BEGIN -->
+        <div style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;">
+            Please view update from Windham Mall.
+        </div>
+        <!-- Visually Hidden Preheader Text : END -->
+
+        <!--
+            Set the email width. Defined in two places:
+            1. max-width for all clients except Desktop Windows Outlook, allowing the email to squish on narrow but never go wider than 600px.
+            2. MSO tags for Desktop Windows Outlook enforce a 600px width.
+        -->
+        <div style="max-width: 600px; margin: auto;" class="email-container">
+            <!--[if mso]>
+            <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" width="600" align="center">
+            <tr>
+            <td>
+            <![endif]-->
+
+            <!-- Email Header : BEGIN -->
+            <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px;">
+                <tr>
+                    <td style="padding: 20px 0; text-align: center">
+                        <!--img src="http://placehold.it/200x50" aria-hidden="true" width="200" height="50" alt="alt_text" border="0" style="height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;"-->
+                    </td>
+                </tr>
+            </table>
+            <!-- Email Header : END -->
+
+            <!-- Email Body : BEGIN -->
+            <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px;">
+
+                <!-- Hero Image, Flush : BEGIN -->
+                <tr>
+                    <td bgcolor="#ffffff">
+                        <img src="media/windham-front.jpg" aria-hidden="true" width="600" height="" alt="alt_text" border="0" align="center" style="width: 100%; max-width: 600px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" class="g-img">
+                    </td>
+                </tr>
+                <!-- Hero Image, Flush : END -->
+
+                <!-- 1 Column Text + Button : BEGIN -->
+                <tr>
+                    <td bgcolor="#ffffff">
+                        <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" width="100%">
+                            <tr>
+                                <td style="padding: 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+                                    <p style="text-align: center">ATTENTION WINDHAM MALL EMPLOYEE:</p>
+                                    {$body}
+                                    <!-- Clear Spacer : BEGIN -->
+                                    <tr>
+                                        <td height="40" style="font-size: 0; line-height: 0;">
+                                            &nbsp;
+                                        </td>
+                                    </tr>
+                                    <!-- Clear Spacer : END -->
+                                    <!-- Button : BEGIN -->
+                                    <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: auto;">
+                                        <tr>
+                                            <td style="border-radius: 3px; background: #222222; text-align: center;" class="button-td">
+                                                <a href="http://windhamschedule.us" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 1.1; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
+                                                    <span style="color:#ffffff;" class="button-link">&nbsp;&nbsp;&nbsp;&nbsp;View Schedule&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                    <!-- Button : END -->
+                <!-- 1 Column Text : BEGIN -->
+                <tr>
+                    <td bgcolor="#ffffff">
+                        <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" width="100%">
+                            <tr>
+                                <td style="padding: 40px; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;">
+                                    If there are any questions regarding this update, please contact <a href="mailto:jay@wrebrokers.com">Jay Wise</a>. Questions about this service? Please contact <a href="mailto:zcopland16@gmail.com">Zach Copland</a>.
+                                </td>
+                                </tr>
+                        </table>
+                    </td>
+                </tr>
+                <!-- 1 Column Text : END -->
+            </table>
+            <!-- Email Body : END -->
+
+            <!-- Email Footer : BEGIN -->
+            <table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 680px;">
+                <tr>
+                    <td style="padding: 40px 10px;width: 100%;font-size: 12px; font-family: sans-serif; line-height:18px; text-align: center; color: #888888;" class="x-gmail-data-detectors">
+                        <br><br>
+                        Windham Mall<br>795 Roosevelt Trl, North Windham, ME, 04062 US<br>(207) 329-3499
+                        <br><br>
+                    </td>
+                </tr>
+            </table>
+            <!-- Email Footer : END -->
+            <!--[if mso]>
+            </td>
+            </tr>
+            </table>
+            <![endif]-->
+        </div>
+    </center>
+</body>
+HTML;
+
+//Set who the message is to be sent from
+$mail->setFrom('zach@zachcopland.com', 'Windham Mall');
+//Set an alternative reply-to address
+$mail->addReplyTo('zcopland16@gmail.com', 'Zach Copland');
+//Set the subject line
+$mail->Subject = 'Windham Mall Update';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML($message);
+//Replace the plain text body with one created manually
+$mail->AltBody = $body;
+
+//send the message, check for errors
+if ($method == 'email' || $method == 'both') {
+    if (!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+        echo "<h3>Email(s) sent!</h3>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Notifications Sent</title>
+    <!-- Start of Bootstrap -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- End of Bootsrap -->
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Notifications have been sent to employees.</h1>
+        <button class="btn vermillion-bg btn-md pull-right"><a href="main.php" class="white-text">Back</a></button>
+    </div>
+</body>
+</html>
