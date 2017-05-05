3a75fe0ba3e6ec976e338ec543b0e39ee5c4d4fb
Zach <zachcopland@Zachs-MacBook-Pro.local>
2017-05-04 23:10:59 -0400

    updated for resetpass.php

--- db/reset.php
+++ db/reset.php
@@ -5,25 +5,37 @@ include 'dbh.php';
 $username = $_POST['username'];
 $password1 = $_POST['password1'];
 $password2 = $_POST['password2'];
-$ver = $_POST['ver'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
 
 if ($password1 == $password2) {
-	//passwords match
-	if ($ver != "1482983673") {
-		//Verification code is incorrect
-		$_SESSION['ver-alert'] = true;
+	$sql = "SELECT * FROM `employees` WHERE `username`='{$username}' AND `email`='{$email}' AND `phone`='{$phone}';";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    
+    if ($row > 0) {
+        /* found row, now update password */
+        $id = $row['id'];
+        $pwd = password_hash($password1, PASSWORD_BCRYPT, array("cost" => 10));
+    	$sql = "UPDATE employees SET password='{$pwd}' WHERE id={$id};";
+        $result2 = mysqli_query($conn, $sql);
+        if ($result2) {
+            $_SESSION['ver-alert'] = false;
+    		$_SESSION['username-alert'] = false;
+    		$_SESSION['pass-alert'] = false;
+            header("Location: ../index.php");
+        }
+        
+    } else if ($row <= 0) {
+        /* could not find email or phone */
+        $_SESSION['ver-alert'] = true;
 		$_SESSION['username-alert'] = false;
 		$_SESSION['pass-alert'] = false;
 		header("Location: ../reset-pass.php");
-	}
-    
-    $pwd = password_hash($password1, PASSWORD_BCRYPT, array("cost" => 10));
-	$sql = "UPDATE employees SET password='$pwd' WHERE username='$username'";
-    $result = mysqli_query($conn, $sql);
-    header("Location: ../index.php");
+    }
 
 } else {
-	//they do not match
+	/* passwords do not match */
 	$_SESSION['pass-alert'] = true;
 	$_SESSION['ver-alert'] = false;
 	$_SESSION['username-alert'] = false;
