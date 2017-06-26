9047d2e2714c69db2b71e9fd7b2f99ccd23aa858
Zach <zachcopland@Zachs-MacBook-Pro.local>
2017-06-25 20:44:12 -0400

    updated title
    schedule —> scheduler

--- main.php
+++ main.php
@@ -58,7 +58,7 @@ $date = (String) date("Y-m-d");
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Schedule</title>
+	<title>Scheduler</title>
 	<meta charset='utf-8' />
 	<!-- Start of Bootstrap -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
